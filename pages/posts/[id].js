9d356bcfb125bb850829372ff532332e4d5fea77
riku0329 user name <riku03290@gmail.com>
2020-07-14 12:49:56 +0900

    edit

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,4 @@
-import Layout from '../../components/Layout';
+import Layout from '../../components/layout';
 import Date from '../../components/date';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 import Head from 'next/head';
