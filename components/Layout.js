43b951e324a2f2aac69282a0facc06124c698d44
riku0329 user name <riku03290@gmail.com>
2020-07-14 12:51:50 +0900

    edit

--- components/Layout.js
+++ components/Layout.js
@@ -6,6 +6,7 @@ import Link from 'next/link';
 const name = '[Your Name]';
 export const siteTitle = 'Next.js Sample Website';
 
+
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
